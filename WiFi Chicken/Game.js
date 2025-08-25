1210aad7dab5bad18129e585980ab0042380b3fd
Soman Rahimi <adaclitch@gmail.com>
2025-08-26 00:07:21 +0430

    Add files via upload

---
+++ WiFi Chicken/Game.js
@@ -0,0 +1,22 @@
+//setup and utils 
+const game = document.getElementById("game");
+game.focus();
+
+function w(){ return game.clientWidth;}
+function H(){ return game.clientHeight;}
+function clamp(v,a,b){
+    return Math.max(a,Math.min(b,v));
+}
+//rand(0,5) ==>3,4
+function rand(min,max){
+    return Math.random()*(max-min)+min;
+}
+
+//choose([1,3,5]) ==>5
+function choose(arr){
+    return arr[ Math.floor(Math.random*arr.length)]
+}
+
+function now(){
+    return performance.now();
+}
